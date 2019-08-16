2df5b02382ef81952adb043c82e69a7484f6c23a
bleakgrey <bleakgrey@gmail.com>
2019-08-16 15:00:17 +0300

    Waypoint Selection GUI

---
+++ src/main/java/preternatural/client/gui/GuiWaypointSelect.java
@@ -0,0 +1,170 @@
+package preternatural.client.gui;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.realmsclient.gui.ChatFormatting;
+import net.minecraft.block.BannerBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.GuiLighting;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.util.NarratorManager;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.math.MathHelper;
+import org.lwjgl.opengl.GL11;
+import preternatural.utils.Waypoint;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static net.minecraft.client.render.VertexFormats.POSITION_COLOR;
+
+public class GuiWaypointSelect extends Screen {
+
+	protected int timeIn = 0;
+	protected int slotSelected = -1;
+	protected ItemStack tool;
+
+	protected HashMap<DyeColor, Waypoint> waypoints = new HashMap<>();
+	protected ArrayList<ItemStack> displayStacks = new ArrayList<>();
+
+	public GuiWaypointSelect(ItemStack stack) {
+		super(NarratorManager.EMPTY);
+		this.tool = stack;
+	}
+
+	@Override
+	protected void init() {
+		DyeColor[] colors = {DyeColor.RED, DyeColor.BLUE, DyeColor.GREEN, DyeColor.YELLOW, DyeColor.WHITE, DyeColor.BLACK};
+		//DyeColor[] colors = DyeColor.values();
+		for (DyeColor color : colors) {
+			waypoints.put(color, new Waypoint());
+			ItemStack banner = new ItemStack(BannerBlock.getForColor(color).asItem());
+			displayStacks.add(banner);
+		}
+	}
+
+	@Override
+	public void render(int mx, int my, float partialTicks) {
+		this.renderBackground();
+
+		int x = width / 2;
+		int y = height / 2;
+		int maxRadius = 80;
+		double angle = mouseAngle(x, y, mx, my);
+		int segments = waypoints.size();
+		float step = (float) Math.PI / 180;
+		float degPer = (float) Math.PI * 2 / segments;
+
+		Tessellator tess = Tessellator.getInstance();
+		BufferBuilder buf = tess.getBufferBuilder();
+		GlStateManager.disableCull();
+		GlStateManager.disableTexture();
+		GlStateManager.enableBlend();
+		//GlStateManager.shadeModel(GL11.GL_SMOOTH);
+		buf.begin(GL11.GL_TRIANGLE_FAN, POSITION_COLOR);
+
+		for(int seg = 0; seg < segments; seg++) {
+			boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
+			float radius = Math.max(0F, Math.min((timeIn + partialTicks - seg * 6F / segments) * 40F, maxRadius));
+			if (mouseInSector)
+				radius *= 1.025f;
+
+			int gs = 0x50;//0x40;
+//			if(seg % 2 == 0)
+//				gs += 0x19;
+			int r = gs;
+			int g = gs;
+			int b = gs;
+			int a = 0x30;
+
+			if(seg == 0)
+				buf.vertex(x, y, 0).color(r, g, b, a).next();
+
+			if(mouseInSector) {
+				slotSelected = seg;
+				r = g = b = 0xFF;
+			}
+
+			for(float i = 0; i < degPer + step / 2; i += step) {
+				float rad = i + seg * degPer;
+				float xp = x + MathHelper.cos(rad) * radius;
+				float yp = y + MathHelper.sin(rad) * radius;
+
+				if (i == 0)
+					buf.vertex(xp, yp, 0).color(r, g, b, a).next();
+				buf.vertex(xp, yp, 0).color(r, g, b, a).next();
+			}
+		}
+		tess.draw();
+
+		GlStateManager.shadeModel(GL11.GL_FLAT);
+		GlStateManager.enableTexture();
+
+		for(int seg = 0; seg < segments; seg++) {
+			boolean mouseInSector = degPer * seg < angle && angle < degPer * (seg + 1);
+			float radius = Math.max(0F, Math.min((timeIn + partialTicks - seg * 6F / segments) * 40F, maxRadius));
+
+			float rad = (seg + 0.5f) * degPer;
+			float xp = x + MathHelper.cos(rad) * radius;
+			float yp = y + MathHelper.sin(rad) * radius;
+
+			ItemStack stack = displayStacks.get(seg);
+			if(!stack.isEmpty()) {
+				float xsp = xp - 4;
+				float ysp = yp;
+				String name = (mouseInSector ? ChatFormatting.RESET : ChatFormatting.GRAY) + stack.getName().asString();
+				int width = font.getStringWidth(name);
+
+				double mod = 0.6;
+				int xdp = (int) ((xp - x) * mod + x);
+				int ydp = (int) ((yp - y) * mod + y);
+
+				MinecraftClient.getInstance().getItemRenderer().renderGuiItem(stack, xdp - 8, ydp - 8);
+
+				if(xsp < x)
+					xsp -= width - 8;
+				if(ysp < y)
+					ysp -= 9;
+
+				font.drawWithShadow(name, xsp, ysp, 0xFFFFFF);
+			}
+		}
+
+		float shift = Math.min(5, timeIn + partialTicks) / 5;
+		float scale = 2 * shift;
+		GlStateManager.enableRescaleNormal();
+		GlStateManager.enableBlend();
+		GlStateManager.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+		GuiLighting.enable();
+
+		GlStateManager.pushMatrix();
+		GlStateManager.scalef(scale, scale, scale);
+
+		MinecraftClient.getInstance().getItemRenderer().renderGuiItem(tool, (int) (x / scale) - 8, (int) (y / scale) - 8);
+		GlStateManager.popMatrix();
+
+		GuiLighting.disable();
+		GlStateManager.disableBlend();
+		GlStateManager.disableRescaleNormal();
+	}
+
+	@Override
+	public boolean isPauseScreen() {
+		return false;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		timeIn++;
+		if(!hasShiftDown())
+			MinecraftClient.getInstance().openScreen(null);
+	}
+
+	private static double mouseAngle(int x, int y, int mx, int my) {
+		return (MathHelper.atan2(my - y, mx - x) + Math.PI * 2) % (Math.PI * 2);
+	}
+
+}
--- src/main/java/preternatural/items/ItemClaymore.java
+++ src/main/java/preternatural/items/ItemClaymore.java
@@ -2,25 +2,28 @@ package preternatural.items;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.block.BannerBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BannerBlockEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Rarity;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import preternatural.Mod;
 import preternatural.ModEntities;
+import preternatural.client.gui.GuiWaypointSelect;
 import preternatural.entities.EntityRift;
 import preternatural.utils.Waypoint;
+import preternatural.utils.WorldUtils;
 
 public class ItemClaymore extends SwordItem {
 
@@ -50,6 +53,10 @@ public class ItemClaymore extends SwordItem {
         BlockState blockState = world.getBlockState(ctxBlockPos);
         PlayerEntity player = ctx.getPlayer();
 
+        if(blockState.getBlock() instanceof BannerBlock) {
+            return onUsedOnBanner(ctx);
+        }
+
         if (world.isClient || player == null)
             return ActionResult.SUCCESS;
 
@@ -67,11 +74,36 @@ public class ItemClaymore extends SwordItem {
         return ActionResult.SUCCESS;
     }
 
+    protected ActionResult onUsedOnBanner(ItemUsageContext ctx) {
+        World world = ctx.getWorld();
+        BlockPos pos = ctx.getBlockPos();
+        BannerBlockEntity entity = (BannerBlockEntity) world.getBlockEntity(pos);
+        BlockState blockState = world.getBlockState(pos);
+        DyeColor color = ((BannerBlock)blockState.getBlock()).getColor();
+        PlayerEntity player = ctx.getPlayer();
+
+        if(entity == null) {
+            Mod.log("No BannerBlockEntity entity found!");
+            return ActionResult.FAIL;
+        }
+
+        if(player.isSneaking()) {
+            WorldUtils.spawnBlockParticles(world, pos, Direction.values(), ParticleTypes.POOF);
+        }
+        else {
+            WorldUtils.spawnBlockParticles(world, pos, Direction.values(), ParticleTypes.PORTAL);
+        }
+
+        Mod.log(color.toString());
+        return ActionResult.SUCCESS;
+    }
+
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
         if (player.isInSneakingPose()) {
-            Mod.log("Show GUI here...");
+            if(world.isClient)
+                MinecraftClient.getInstance().openScreen(new GuiWaypointSelect(stack));
             return new TypedActionResult(ActionResult.SUCCESS, stack);
         }
         else
